bf5cc73fe45ad8ac214b853e95e199b5e8382de0
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-09-27 14:41:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  TOKEN: "NzU5NDQ5ODg3MzQyMzI5ODc2.X29q0w.hgeLwnT7iK8mTvFbMxVjDnDm-hg",
-  DBCONNECTION: "mongodb+srv://sonmaximexp:Maxime19052005@zeldaxp.vaaf4.mongodb.net/zeldaxp?retryWrites=true&w=majority",
+  TOKEN: "token",
+  DBCONNECTION: "mongodb",
   DEFAULTSETTINGS: {
     prefix: "!"
   }
