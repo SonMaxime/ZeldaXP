b422d59bdd6c52ec831e3a1a053601212d130bf1
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-03-25 09:54:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const { Client, Collection } = require('discord.js');
 const { loadCommands, loadEvents } = require("./util/loader");
+const { Database } = require("bookman");
 
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });;
+const client = new Client({
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'MEMBER'],
+    restTimeOffset: 0
+});
 require("./util/functions")(client);
 client.config = require("./config");
 client.queue = new Map();
 client.mongoose = require("./util/mongoose");
+client.db = new Database("database/languageGuild.json");
 ["commands", "cooldowns"].forEach(x => client[x] = new Collection());
 
 loadCommands(client);
